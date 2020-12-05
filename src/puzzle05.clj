05192ae61d26ee8326d8c8ed95f5a9503fd69f35
Joanne Cheng <chengjoanne2@gmail.com>
2020-12-05 10:17:59 -0500

    use binary to parse code

--- src/puzzle05.clj
+++ src/puzzle05.clj
@@ -22,12 +22,22 @@
         (#{\R \B} cur-str) (recur (rest seat-str) (take-upper-half input))
         :else (first input)))))
 
+(defn parse-code-binary [seat-str]
+  (->> seat-str
+       (map-indexed
+        (fn [i cur]
+          (if (#{\R \B} cur)
+            (Math/pow 2 (- (dec (count seat-str)) i))
+            0)))
+       (reduce +)))
+
 (defn decode-row-seat-code [row-seat-code]
   (let [row-code (subs row-seat-code 0 7)
         seat-code (subs row-seat-code 7)]
-    (+
-     (* 8 (parse-code row-code))
-     (parse-code seat-code))))
+    (int
+     (+
+      (* 8 (parse-code-binary row-code))
+      (parse-code-binary seat-code)))))
 
 (def seats
   (map
